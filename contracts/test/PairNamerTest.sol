9e5742845213f80eba8664dc762153a9bcabec64
Moody Salem <moody.salem@gmail.com>
2020-04-21 12:14:52 -0500

    Pair namer test, remove strings.sol used only for concatenation

---
+++ contracts/test/PairNamerTest.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.5.0;
+
+import '../libraries/PairNamer.sol';
+
+contract PairNamerTest {
+    function pairName(address token0, address token1, string calldata prefix, string calldata suffix) external view returns (string memory) {
+        return PairNamer.pairName(token0, token1, prefix, suffix);
+    }
+
+    function pairSymbol(address token0, address token1, string calldata suffix) external view returns (string memory) {
+        return PairNamer.pairSymbol(token0, token1, suffix);
+    }
+}
+
+// we do not test with non compliant ERC20 implementations because that's handled by token namer
+contract PairNamerTestFakeToken {
+    string public name;
+    string public symbol;
+    constructor(string memory name_, string memory symbol_) public {
+        name = name_;
+        symbol = symbol_;
+    }
+}
