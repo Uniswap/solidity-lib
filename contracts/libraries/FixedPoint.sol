9642a0705fdaf36b477354a4167a8cd765250860
Noah Zinsmeister <noahwz@gmail.com>
2020-06-26 13:53:27 -0400

    add SPDX (#3)
    * add SPDX-License-Identifier comments
    
    * add unlicensed to test contracts
    
    * use GPL for everything

--- contracts/libraries/FixedPoint.sol
+++ contracts/libraries/FixedPoint.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 pragma solidity >=0.4.0;
 
 import './Babylonian.sol';
--- contracts/libraries/TransferHelper.sol
+++ contracts/libraries/TransferHelper.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 pragma solidity >=0.6.0;
 
 // helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
