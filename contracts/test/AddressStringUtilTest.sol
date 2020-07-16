f80e45e0065af01e2ead73df1058214a2fd86488
Noah Zinsmeister <noahwz@gmail.com>
2020-07-16 12:16:53 -0400

    fix some lint errors
    uint256 -> uint

--- contracts/test/AddressStringUtilTest.sol
+++ contracts/test/AddressStringUtilTest.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.5.0;
 import "../libraries/AddressStringUtil.sol";
 
 contract AddressStringUtilTest {
-    function toAsciiString(address addr, uint len) pure external returns (string memory) {
+    function toAsciiString(address addr, uint len) external pure returns (string memory) {
         return AddressStringUtil.toAsciiString(addr, len);
     }
 }
