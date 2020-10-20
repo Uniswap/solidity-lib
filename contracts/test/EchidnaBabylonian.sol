ea30fae7b6e6851d6ae92231e285add9d45a8000
Moody Salem <moodysalem@users.noreply.github.com>
2020-10-20 16:21:41 -0500

    first fuzz test (#16)
    * first fuzz test
    
    * sqrt always less than max uint
    
    * stricter bound
    
    * Update contracts/test/EchidnaBabylonian.sol
    
    Co-authored-by: Noah Zinsmeister <noahwz@gmail.com>
    
    * sanity check a case that is expected to fail for max uint
    
    * rename job
    
    * check for overflow
    
    * try forcing failure
    
    * Revert "try forcing failure"
    
    This reverts commit 4c87bb48
    
    * remove the assert
    
    Co-authored-by: Noah Zinsmeister <noahwz@gmail.com>

---
+++ contracts/test/EchidnaBabylonian.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.4.0;
+
+import '../libraries/Babylonian.sol';
+
+contract EchidnaBabylonianTest {
+    uint256 num;
+    uint256 sqrt;
+
+    function storeSqrt(uint256 num_) external {
+        num = num_;
+        sqrt = Babylonian.sqrt(num_);
+    }
+
+    function echidna_sqrtAlwaysLessThanMaxUint128() external view returns (bool) {
+        return sqrt < 2**128; // because (2**128)^2 > uint256(-1)
+    }
+
+    function echidna_sqrtSquaredAlwaysLteInput() external view returns (bool) {
+        return sqrt * sqrt <= num;
+    }
+
+    function echidna_sqrtPlusOneSquaredAlwaysGtInput() external view returns (bool) {
+        uint256 next = sqrt + 1;
+        uint256 nextSquared = next * next;
+        return (nextSquared > num); /*|| ((nextSquared) / next != next)*/
+    }
+}
