0663fa10fef98f17bb2dfc123b6ba8325ee51973
Moody Salem <moody.salem@gmail.com>
2020-10-20 14:32:36 -0500

    first fuzz test

---
+++ contracts/test/EchidnaBabylonian.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.4.0;
+
+import '../libraries/Babylonian.sol';
+
+contract EchidnaBabylonianTest {
+    uint256 num;
+    uint256 sqrt;
+
+    function storeSqrt(uint256 num_) external {
+        num = num_;
+        sqrt = Babylonian.sqrt(num_);
+    }
+
+    function echidna_sqrtCheck() external view returns (bool) {
+        uint256 next = sqrt + 1;
+        return (sqrt * sqrt) <= num && (next * next > num || (next * next < next));
+    }
+}
