cf8603f67786981f6c673e975a92a55c87085f68
Moody Salem <moody.salem@gmail.com>
2020-11-04 18:52:06 -0600

    add asserts

--- contracts/test/BabylonianEchidnaTest.sol
+++ contracts/test/BabylonianEchidnaTest.sol
@@ -14,16 +14,19 @@ contract BabylonianEchidnaTest {
     }
 
     function echidna_sqrtAlwaysLessThanMaxUint128() external view returns (bool) {
+        assert(sqrt < 2**128);
         return sqrt < 2**128; // because (2**128)^2 > uint256(-1)
     }
 
     function echidna_sqrtSquaredAlwaysLteInput() external view returns (bool) {
+        assert(sqrt * sqrt <= input);
         return sqrt * sqrt <= input;
     }
 
     function echidna_sqrtPlusOneSquaredAlwaysGtInput() external view returns (bool) {
         uint256 next = sqrt + 1;
         uint256 nextSquared = next * next;
+        assert(nextSquared > input);
         return (nextSquared > input); /*|| ((nextSquared) / next != next)*/
     }
 }
