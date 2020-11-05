1354ad7a6d94549c4dee49193e120266acfc5eb8
Moody Salem <moody.salem@gmail.com>
2020-11-05 09:01:16 -0600

    simplify test

--- contracts/test/BabylonianEchidnaTest.sol
+++ contracts/test/BabylonianEchidnaTest.sol
@@ -5,28 +5,10 @@ pragma solidity >=0.4.0;
 import '../libraries/Babylonian.sol';
 
 contract BabylonianEchidnaTest {
-    uint256 input;
-    uint256 sqrt;
-
-    function storeSqrt(uint256 input_) external {
-        input = input_;
-        sqrt = Babylonian.sqrt(input_);
-    }
-
-    function echidna_sqrtAlwaysLessThanMaxUint128() external view returns (bool) {
+    function checkSqrt(uint256 input) external {
+        uint256 sqrt = Babylonian.sqrt(input_);
         assert(sqrt < 2**128);
-        return sqrt < 2**128; // because (2**128)^2 > uint256(-1)
-    }
-
-    function echidna_sqrtSquaredAlwaysLteInput() external view returns (bool) {
         assert(sqrt * sqrt <= input);
-        return sqrt * sqrt <= input;
-    }
-
-    function echidna_sqrtPlusOneSquaredAlwaysGtInput() external view returns (bool) {
-        uint256 next = sqrt + 1;
-        uint256 nextSquared = next * next;
         assert(nextSquared > input);
-        return (nextSquared > input); /*|| ((nextSquared) / next != next)*/
     }
 }
