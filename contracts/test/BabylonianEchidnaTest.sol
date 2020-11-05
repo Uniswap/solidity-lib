7924684ba4688e66daa9244c51c28d54b2f14e68
Moody Salem <moody.salem@gmail.com>
2020-11-05 09:04:03 -0600

    fix compile error

--- contracts/test/BabylonianEchidnaTest.sol
+++ contracts/test/BabylonianEchidnaTest.sol
@@ -5,8 +5,9 @@ pragma solidity >=0.4.0;
 import '../libraries/Babylonian.sol';
 
 contract BabylonianEchidnaTest {
-    function checkSqrt(uint256 input) external {
-        uint256 sqrt = Babylonian.sqrt(input_);
+    function checkSqrt(uint256 input) external pure {
+        uint256 sqrt = Babylonian.sqrt(input);
+        uint256 nextSquared = (sqrt + 1)**2;
         assert(sqrt < 2**128);
         assert(sqrt * sqrt <= input);
         assert(nextSquared > input);
