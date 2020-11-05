fa6d83ea1b6beef51f91969fd0bd3e99db5fc2eb
Moody Salem <moody.salem@gmail.com>
2020-11-05 09:14:32 -0600

    check the max

--- contracts/test/BabylonianEchidnaTest.sol
+++ contracts/test/BabylonianEchidnaTest.sol
@@ -5,17 +5,16 @@ pragma solidity >=0.4.0;
 import '../libraries/Babylonian.sol';
 
 contract BabylonianEchidnaTest {
-    function checkMaxForIndex(uint8 index) {
-        checkSqrt(2**index);
-        checkSqrt(index == 255 ? uint256(-1) : 2**(index + 1));
-    }
-
-    function checkSqrt(uint256 input) external pure {
+    function checkSqrt(uint256 input) public pure {
         uint256 sqrt = Babylonian.sqrt(input);
 
         assert(sqrt < 2**128); // 2**128 == sqrt(2^256)
         // since we compute floor(sqrt(input))
         assert(sqrt**2 <= input);
         assert((sqrt + 1)**2 > input);
+    }
+
+    function checkMaxForIndex(uint8 index) external pure {
+        checkSqrt(index == 255 ? uint256(-1) : uint256(2)**(index + 1));
     }
 }
