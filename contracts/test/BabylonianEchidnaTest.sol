6761ae1981c868e4b4597d7b4744cf341270c02c
Moody Salem <moody.salem@gmail.com>
2020-11-11 17:36:02 -0600

    better test

--- contracts/test/BabylonianEchidnaTest.sol
+++ contracts/test/BabylonianEchidnaTest.sol
@@ -5,25 +5,16 @@ pragma solidity >=0.4.0;
 import '../libraries/Babylonian.sol';
 
 contract BabylonianEchidnaTest {
-    uint256 input;
-    uint256 sqrt;
+    function checkSqrt(uint256 input) public pure {
+        uint256 sqrt = Babylonian.sqrt(input);
 
-    function storeSqrt(uint256 input_) external {
-        input = input_;
-        sqrt = Babylonian.sqrt(input_);
+        assert(sqrt < 2**128); // 2**128 == sqrt(2^256)
+        // since we compute floor(sqrt(input))
+        assert(sqrt**2 <= input);
+        assert((sqrt + 1)**2 > input || sqrt == uint128(-1));
     }
 
-    function echidna_sqrtAlwaysLessThanMaxUint128() external view returns (bool) {
-        return sqrt < 2**128; // because (2**128)^2 > uint256(-1)
-    }
-
-    function echidna_sqrtSquaredAlwaysLteInput() external view returns (bool) {
-        return sqrt * sqrt <= input;
-    }
-
-    function echidna_sqrtPlusOneSquaredAlwaysGtInput() external view returns (bool) {
-        uint256 next = sqrt + 1;
-        uint256 nextSquared = next * next;
-        return (nextSquared > input); /*|| ((nextSquared) / next != next)*/
+    function checkMaxForIndex(uint8 index) external pure {
+        checkSqrt(index == 255 ? uint256(-1) : uint256(2)**(index + 1));
     }
 }
--- contracts/test/SquareRootEchidnaTest.sol
+++ contracts/test/SquareRootEchidnaTest.sol
@@ -5,25 +5,16 @@ pragma solidity >=0.4.0;
 import '../libraries/SquareRoot.sol';
 
 contract SquareRootEchidnaTest {
-    uint256 input;
-    uint256 sqrt;
+    function checkSqrt(uint256 input) public pure {
+        uint256 sqrt = SquareRoot.sqrt(input);
 
-    function storeSqrt(uint256 input_) external {
-        input = input_;
-        sqrt = SquareRoot.sqrt(input_);
+        assert(sqrt < 2**128); // 2**128 == sqrt(2^256)
+        // since we compute floor(sqrt(input))
+        assert(sqrt**2 <= input);
+        assert((sqrt + 1)**2 > input || sqrt == uint128(-1));
     }
 
-    function echidna_sqrtAlwaysLessThanMaxUint128() external view returns (bool) {
-        return sqrt < 2**128; // because (2**128)^2 > uint256(-1)
-    }
-
-    function echidna_sqrtSquaredAlwaysLteInput() external view returns (bool) {
-        return sqrt * sqrt <= input;
-    }
-
-    function echidna_sqrtPlusOneSquaredAlwaysGtInput() external view returns (bool) {
-        uint256 next = sqrt + 1;
-        uint256 nextSquared = next * next;
-        return (nextSquared > input); /*|| ((nextSquared) / next != next)*/
+    function checkMaxForIndex(uint8 index) external pure {
+        checkSqrt(index == 255 ? uint256(-1) : uint256(2)**(index + 1));
     }
 }
