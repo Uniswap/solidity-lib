88f21fe544d8daeef400425ce92206c8e3bce033
Moody Salem <moody.salem@gmail.com>
2020-11-12 10:27:13 -0600

    pull unrelated changes

--- contracts/test/BabylonianTest.sol
+++ contracts/test/BabylonianTest.sol
@@ -8,4 +8,10 @@ contract BabylonianTest {
     function sqrt(uint256 num) external pure returns (uint256) {
         return Babylonian.sqrt(num);
     }
+
+    function getGasCostOfSqrt(uint256 num) external view returns (uint256) {
+        uint256 gasBefore = gasleft();
+        Babylonian.sqrt(num);
+        return gasBefore - gasleft();
+    }
 }
